5a6e233eb94f80305340c5c7fdafe709df6d5bac
Egil Elias Ravnå Birkeland <e.e.r.birkeland@usit.uio.no>
2021-02-21 14:55:12 +0100

    Legger til ryggkart.

--- pages/bodymap/back.tsx
+++ pages/bodymap/back.tsx
@@ -1,3 +1,12 @@
+import MapContainer from '../../src/components/bodyMaps/MapContainer';
+import { back } from '../../src/components/bodyMaps/mapData';
+import { MessageBoxBodyMap } from '../../src/components/messageBoxes';
+
 export default function Back() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <MessageBoxBodyMap />
+      <MapContainer map={back.default} />
+    </div>
+  );
 }
--- src/components/bodyMaps/mapData/index.ts
+++ src/components/bodyMaps/mapData/index.ts
@@ -1,11 +1,13 @@
 import * as wholeBody from './wholeBody';
 import * as head from './headMap';
+import * as back from './backMap';
 
 const bodyMaps = {
   wholeBody: wholeBody.default,
   headJawMouth: head.default,
+  backMap: back.default,
 };
 
-export { wholeBody, head, bodyMaps };
+export { wholeBody, head, bodyMaps, back };
 
 export default bodyMaps;
