a920e3bd3de10f5be56f2d6af132a603ce5d171d
Egil Elias Ravnå Birkeland <e.e.r.birkeland@usit.uio.no>
2021-02-21 15:13:44 +0100

    Left arm map.

--- src/components/bodyMaps/mapData/index.ts
+++ src/components/bodyMaps/mapData/index.ts
@@ -1,13 +1,15 @@
 import * as wholeBody from './wholeBody';
 import * as head from './headMap';
 import * as back from './backMap';
+import * as leftArm from './leftArmMap';
 
 const bodyMaps = {
   wholeBody: wholeBody.default,
   headJawMouth: head.default,
   backMap: back.default,
+  leftArm: leftArm.default,
 };
 
-export { wholeBody, head, bodyMaps, back };
+export { wholeBody, head, bodyMaps, back, leftArm };
 
 export default bodyMaps;
