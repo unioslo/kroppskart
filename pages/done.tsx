5c979650b81e6fb306f101b4c77521b44841ebfa
Egil Elias Ravnå Birkeland <eebirke@uio.no>
2023-05-03 16:25:28 +0200

    Legger til @uio-appu/nettskjema-utils. Tester bytte til API.

--- pages/done.tsx
+++ pages/done.tsx
@@ -1,34 +1,15 @@
 import { useRouter } from 'next/router';
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import useSWR from 'swr';
 import { MessageBoxDelivering } from '../src/components/messageBoxes';
 import { rootState } from '../src/store/store';
-import {
-  postSubmission,
-  submissionFromAnswerState,
-} from '../src/utils/submissionUtils';
+import { submissionFromAnswerState } from '../src/utils/submissionUtils';
 import { resetAppState } from '../src/store/appStateReducer';
 import { resetBodyMaps } from '../src/store/bodyMapReducer';
 import { getUrlParam } from '../src/utils/routingUtils';
 import { Alert, Button } from '../src/components/ui';
 import { allAnswersFalse } from '../src/utils/mapUtils';
-
-const useGetForm = (formId?: string) => {
-  const { data, error } = useSWR(
-    formId ? `/kroppskart/api/codebook?formId=${formId}` : null,
-    (url: string) =>
-      fetch(url)
-        .then((r) => r.json())
-        .then((j) => j.codebook)
-  );
-
-  return {
-    codebookMap: data,
-    isLoading: !data && !error,
-    error,
-  };
-};
+import { createSubmissionAndPost } from '@uio-appu/nettskjema-utils';
 
 const Done = () => {
   const dispatch = useDispatch();
@@ -46,17 +27,10 @@ const Done = () => {
   const [delivering, setDelivering] = React.useState(false);
   const [failed, setFailed] = React.useState(false);
   const [delivered, setDelivered] = React.useState(false);
-
-  const { codebookMap } = useGetForm(dataTarget);
-
+  const [isClient, setIsClient] = React.useState(typeof window !== 'undefined');
   let submission;
-  if (initialized && codebookMap) {
-    submission = submissionFromAnswerState(
-      maps,
-      sex,
-      submissionId,
-      codebookMap
-    );
+  if (initialized && typeof window !== 'undefined') {
+    submission = submissionFromAnswerState(maps, sex, submissionId);
   }
 
   // for (const [key, value] of submission.entries()) {
@@ -64,23 +38,24 @@ const Done = () => {
   // }
 
   React.useEffect(() => {
-    // TODO: Reenable to deliver data
-    const hasSufficientData = !!(codebookMap && submission && dataTarget);
+    if (!isClient) {
+      setIsClient(typeof window !== 'undefined');
+    }
+    const hasSufficientData = !!(submission && dataTarget);
     const shouldDeliver = !failed && !delivering && !delivered;
 
-    if (hasSufficientData && shouldDeliver) {
+    if (isClient && hasSufficientData && shouldDeliver) {
       try {
-        postSubmission(dataTarget, submission, submissionId)
-          .then((res) => res.json())
-          .then((json) => {
-            if (json.status === 'success') {
-              setDelivered(true);
-              setDelivering(false);
-              setFailed(false);
-            } else {
-              setFailed(true);
-              setDelivering(false);
-            }
+        createSubmissionAndPost(dataTarget, submission, 'prod')
+          .then(() => {
+            setDelivered(true);
+            setDelivering(false);
+            setFailed(false);
+          })
+          .catch((e) => {
+            setFailed(true);
+            setDelivering(false);
+            console.error(e);
           });
       } catch (e) {
         setFailed(true);
@@ -88,7 +63,7 @@ const Done = () => {
       }
     }
   }, [
-    codebookMap,
+    isClient,
     delivering,
     failed,
     delivered,
--- src/utils/submissionUtils.ts
+++ src/utils/submissionUtils.ts
@@ -7,20 +7,12 @@ import {
 } from './codebook';
 import { redundantBodyMapKeys, routingMap } from './constants';
 
-const generalAnswer = (id: string, type: 'textAnswer' | 'answerOption') =>
-  `answersAsMap[${id}].${type}`;
-
-//const optionAnswer = (id: string) => generalAnswer(id, 'answerOption');
-
-const textAnswer = (id: string) => generalAnswer(id, 'textAnswer');
-
 const appendSubmapToFormData = (
   map: Record<string, boolean>,
   mapName: string,
   mapSelected: boolean,
   sex: string,
-  formData: FormData,
-  codebookMap: Record<string, string>
+  answers: Record<string, string>
 ) => {
   let codebookMapName = mapName;
   if (mapName === 'genitals' || mapName === 'chest') {
@@ -30,9 +22,9 @@ const appendSubmapToFormData = (
   Object.entries(map).forEach(([key, value]) => {
     const codebookKey = getCodebookKeyForRegion(mapName, codebookMapName, key);
     if (mapSelected && value && !redundantKeys?.includes(key)) {
-      formData.append(textAnswer(codebookMap[codebookKey]), '1');
+      answers[codebookKey] = '1';
     } else if ((!mapSelected || !value) && !redundantKeys?.includes(key)) {
-      formData.append(textAnswer(codebookMap[codebookKey]), '0');
+      answers[codebookKey] = '0';
     }
   });
 };
@@ -40,8 +32,7 @@ const appendSubmapToFormData = (
 const appendMissingSubmapToFormData = (
   mapName: string,
   sex: string,
-  formData: FormData,
-  codebookMap: Record<string, string>
+  answers: Record<string, string>
 ) => {
   let codebookMapName = mapName;
   if (mapName === 'genitals' || mapName === 'chest') {
@@ -49,27 +40,22 @@ const appendMissingSubmapToFormData = (
   }
   Object.entries(codebook[codebookMapName].areas).forEach(([key]) => {
     const codebookKey = getCodebookKeyForRegion(mapName, codebookMapName, key);
-    formData.append(textAnswer(codebookMap[codebookKey]), '0');
+    answers[codebookKey] = '0';
   });
 };
 
 export const submissionFromAnswerState = (
   body: BodyMapState,
   sex: string,
-  submissionId?: string,
-  codebookMap?: Record<string, string>
+  submissionId?: string
 ) => {
-  const formData = new FormData();
-  formData.append(
-    textAnswer(codebookMap.SUBMISSION_REFERENCE),
-    submissionId ?? ''
-  );
-  const userAgent =
+  const answers: Record<string, string> = {};
+  answers['SUBMISSION_REFERENCE'] = submissionId ?? '';
+  answers['HBMP_BROWSER'] =
     typeof window !== 'undefined' ? window?.navigator?.userAgent : 'unknown';
-  formData.append(textAnswer(codebookMap.HBMP_BROWSER), userAgent);
   const date = new Date().toISOString();
-  formData.append(textAnswer(codebookMap.HBMP_DATE), date);
-  formData.append(textAnswer(codebookMap.HBMP_VERSION), 'Kroppskart 2 Next.js');
+  answers['HBMP_DATE'] = date;
+  answers['HBMP_VERSION'] = 'Kroppskart 2 Next.js';
 
   const redundantKeys = redundantBodyMapKeys.wholeBody;
 
@@ -78,40 +64,15 @@ export const submissionFromAnswerState = (
     if (!redundantKeys.includes(key)) {
       const mapName = routingMap[key];
       const codebookKey = getCodebookKeyForMap(key);
-      formData.append(textAnswer(codebookMap[codebookKey]), value ? '1' : '0');
+      answers[codebookKey] = value ? '1' : '0';
       const submap = body[mapName];
       if (value && submap) {
-        appendSubmapToFormData(
-          submap,
-          mapName,
-          value,
-          sex,
-          formData,
-          codebookMap
-        );
+        appendSubmapToFormData(submap, mapName, value, sex, answers);
       } else {
-        appendMissingSubmapToFormData(mapName, sex, formData, codebookMap);
+        appendMissingSubmapToFormData(mapName, sex, answers);
       }
     }
   });
 
-  return formData;
-};
-
-export const postSubmission = (
-  formId: string,
-  body: FormData,
-  submissionId?: string
-) => {
-  const referenceId = submissionId ? `&referenceId=${submissionId}` : '';
-  return fetch(
-    `https://nettskjema.no/answer/deliver.json?formId=${formId}${referenceId}`,
-    {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-      },
-      body,
-    }
-  );
+  return answers;
 };
